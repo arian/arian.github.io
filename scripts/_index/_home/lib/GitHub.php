75f4793240524b6b089fbef63c41e75548ad5493
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-05-25 19:07:39 +0200

    Fix GitHub API fetcher, needs User Agent string

--- scripts/_index/_home/lib/GitHub.php
+++ scripts/_index/_home/lib/GitHub.php
@@ -24,7 +24,8 @@ class GitHub {
 	protected function fetchFromAPI(){
 
 		$ch = curl_init($this->url);
-		curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 		$json = curl_exec($ch);
 
 		if (curl_errno($ch)){
