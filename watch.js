dfc49633c047701a1bcd0ecbdb785d394e0f0f5f
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-10-28 15:59:05 +0100

    Some fixes after updating hexo to 2.3

--- watch.js
+++ watch.js
@@ -1,14 +1,59 @@
 
 var watch = require("node-watch");
 var exec = require("child_process").exec;
+var async = require('async');
+var fs = require('fs');
 
 function build() {
-  exec("./node_modules/.bin/hexo generate", {
-    cwd: __dirname
-  }, function(err) {
+  async.series([
+
+    function(callback) {
+      exec("./node_modules/.bin/hexo generate", {
+        cwd: __dirname
+      }, function(err) {
+        if (err) return callback(err);
+        console.log("generated blog");
+        callback();
+      });
+    },
+
+    function(callback) {
+      var htmlFile = __dirname + "/public/index.html";
+      var phpFile = __dirname + "/public/index.php";
+
+      exec("cp -R _index/* public", {
+        cwd: __dirname
+      }, function(err) {
+        if (err) return callback(err);
+
+        console.log("done copying PHP");
+
+        var html = fs.readFileSync(htmlFile, "utf-8");
+        var php = fs.readFileSync(phpFile);
+
+        php = html.replace('{{content}}', php);
+
+        fs.writeFileSync(phpFile, php);
+        fs.unlink(htmlFile);
+
+        callback();
+
+      });
+    },
+
+    function(callback) {
+      exec("./node_modules/.bin/stylus themes/light/source/css/style.styl --include ./node_modules/nib/lib/ -o public/css/", {
+        cwd: __dirname
+      }, function(err) {
+        if (err) return callback(err);
+        console.log("generated css");
+        callback();
+      });
+    }
+  ], function(err){
     if (err) throw err;
-	console.log("generated blog");
-  });
+    console.log('done');
+  })
 }
 
 watch(__dirname + "/source", build);
